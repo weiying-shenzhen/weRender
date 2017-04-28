5ef98287dd69f0a718c0bb67acaee6e2a196d3a4
henryluki <iamamiracle55@gmail.com>
2017-04-28 18:17:37 +0800

    babel build for class property

--- build/es6.config.js
+++ build/es6.config.js
@@ -1,4 +1,5 @@
 import eslint from 'rollup-plugin-eslint';
+import babel from 'rollup-plugin-babel';
 
 export default {
   entry: 'src/index.js',
@@ -8,6 +9,10 @@ export default {
       exclude: [
         'src/styles/**'
       ]
+    }),
+    babel({
+      babelrc: false,
+      plugins: ["external-helpers", "transform-class-properties"],
     })
   ],
   dest: 'dist/weRender.es6.js'
